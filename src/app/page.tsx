d8ca322139b306ff40f00a8c55aea39709806c28
ANOOP KUMAR <AnoopAKR1@gmail.com>
2025-09-21 11:53:21 +0000

    take me to the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,13 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAppContext } from '@/contexts/app-context';
 
 export default function HomePage() {
   const router = useRouter();
-  const { isLoaded, mode } = useAppContext();
 
   useEffect(() => {
-    if (isLoaded) {
-      if (mode) {
-        router.replace('/dashboard');
-      } else {
-        router.replace('/select-role');
-      }
-    }
-  }, [isLoaded, mode, router]);
+    router.replace('/select-role');
+  }, [router]);
 
   return null;
 }

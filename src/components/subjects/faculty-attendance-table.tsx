0ba69d378810509902e34a713238f3740dae7028
ANOOP KUMAR <AnoopAKR1@gmail.com>
2025-11-06 05:55:37 +0000

    reduce the size of attendence verification windo

--- src/components/subjects/faculty-attendance-table.tsx
+++ src/components/subjects/faculty-attendance-table.tsx
@@ -400,7 +400,7 @@ export function FacultyAttendanceTable({ subject, isAttendanceActive }: { subjec
                 </div>
             </CardHeader>
             <CardContent>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 rounded-lg bg-muted p-4">
+                <div className="grid grid-cols-1 gap-4 rounded-lg bg-muted p-4">
                      <div className="text-center">
                         <p className="text-3xl font-bold flex items-center justify-center gap-2">
                            <Camera/> {cameraHeadcount ?? '-'}
